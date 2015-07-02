415f5f158fa4629052cfef5da9116fb33530902b
Marcelo Sampaio <marcelo@artlogica.com.br>
2015-07-02 19:20:31 -0300

    Initial Commit

---
+++ CardMenu/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CardMenu
+//
+//  Created by Marcelo Sampaio on 7/2/15.
+//  Copyright (c) 2015 Marcelo Sampaio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CardMenu/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CardMenu
+//
+//  Created by Marcelo Sampaio on 7/2/15.
+//  Copyright (c) 2015 Marcelo Sampaio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
